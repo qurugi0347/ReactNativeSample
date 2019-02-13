b7421b182876cfc24e87a1f5782bb48c7669168e
jun <qurugi0347@gmail.com>
2019-02-13 15:45:10 +0900

    init : first build success setting

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import { AppRegistry } from 'react-native';
-import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const React = require('react-native');
+
+const {
+
+  AppRegistry
+
+} = React;
+const Main = require('./src/main');
+
+AppRegistry.registerComponent(appName, () => Main);
