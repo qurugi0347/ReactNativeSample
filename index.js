66fced4d3b074248eed46bc6b6edfca6c3e92b5e
jun <qurugi0347@gmail.com>
2019-02-13 15:49:02 +0900

    fix : build error fix

--- index.js
+++ index.js
@@ -3,15 +3,8 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import { name as appName } from './app.json';
+ import { AppRegistry } from 'react-native';
+ import App from './App';
+ import { name as appName } from './app.json';
 
-const React = require('react-native');
-
-const {
-
-  AppRegistry
-
-} = React;
-const Main = require('./src/main');
-
-AppRegistry.registerComponent(appName, () => Main);
+ AppRegistry.registerComponent(appName, () => App);
