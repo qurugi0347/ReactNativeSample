97dd7449ad5d84ccff2ea289611a92cb100c0521
jun <qurugi0347@gmail.com>
2019-02-13 14:15:12 +0900

    init : eslint native setting

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+/**
+ * @format
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+import { name as appName } from './app.json';
+
+const React = require('react-native');
+
+const {
+  AppRegistry
+} = React;
+
+const Main = require('./src/main');
+
+
+AppRegistry.registerComponent(appName, () => Main);
